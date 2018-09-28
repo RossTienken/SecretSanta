4bf96b2bf51b2d569ec6fa544d3ec6e26e90e4f5
Ross <ross.tienken99@gmail.com>
2018-09-28 15:52:13 -0600

    Secret Santa Part 3

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const assignSantas = (array) => {
 
   let santas = array.slice();
 
-  for(var i=0; i<santas.length; i++) {
-    var santa = santas[i],
+  for(let i=0; i<santas.length; i++) {
+    let santa = santas[i],
         recipient;
 
     // Assign santa to the person next to them to avoid assigning to self and avoid duplicate recipients
@@ -28,7 +28,7 @@ const assignSantas = (array) => {
     matches.push({ "santa": santa, "recipient": recipient });
   }
 
-  console.log(matches)
+  return matches
 }
 
 
@@ -52,8 +52,8 @@ const assignSantas2 = (array) => {
   let year = 1
   while (year < 6) {
     let matches = {};
-    for(var i=0; i<santas.length; i++) {
-      var santa = santas[i],
+    for(let i=0; i<santas.length; i++) {
+      let santa = santas[i],
           recipient;
 
       // Assign santa to the person next to them, and increment +1 for each year, to avoid assigning to self and avoid duplicate recipients
@@ -81,3 +81,65 @@ assignSantas2(['Sean', 'Winnie', 'Brian', 'Amy', 'Samir', 'Joe', 'Bethany', 'Bru
 
 
 // As your extended family has grown, members have gotten married and/or had children. Families usually get gifts for members of their immediate family, so it doesn’t make a lot of sense for anyone to be a Secret Santa for a member of their immediate family (spouse, parents, or children). Modify your program to take this constraint into consideration when choosing Secret Santas.
+
+
+const assignSantas3 = (people) => {
+  let shuffle = function shuffle(array) {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  };
+  let assignPartners = function(people) {
+    let peopleRemaining = people.map(person => person.name);
+
+    people.forEach(function(person) {
+      let choices = peopleRemaining.filter(function(reciever) {
+        return (
+          reciever !== person.name &&
+          !person.family.includes(reciever) &&
+          person.past !== reciever
+        );
+      });
+      shuffle(choices);
+      person.current = choices[0];
+      let index = peopleRemaining.indexOf(choices[0]);
+      peopleRemaining.splice(index, 1);
+    });
+  };
+
+  let allAssigned = false;
+  let loopCount = 0;
+  while (!allAssigned) {
+    assignPartners(people);
+    allAssigned = people.every((person) => person.current);
+    //exit loop if going too long
+    if (loopCount > 50) {
+      throw 'Something went wrong with the assignment';
+    }
+    if (!allAssigned) {
+    people.forEach((person) => person.current = '')
+    }
+  }
+  return people
+}
+
+assignSantas3([
+  {name: 'Sean', family: ['Winnie', 'Brian'], current: '', past: 'Amy'},
+  {name: 'Winnie', family: ['Sean', 'Brian'], current: '', past: 'Joe'},
+  {name: 'Brian', family: ['Winnie', 'Sean'], current: '', past: 'Bruno'},
+  {name: 'Amy', family: ['Joe'], current: '', past: 'Winnie'},
+  {name: 'Joe', family: ['Amy'], current: '', past: 'Sean'},
+  {name: 'Bruno',family: [],current: '',past: 'Brian'}
+])
